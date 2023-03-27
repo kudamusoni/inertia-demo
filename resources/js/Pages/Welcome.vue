73a15ccff4eeea52e4d8cc76d6275814d81c0b56
Kuda <kudamusoni7@gmail.com>
2023-03-27 22:07:59 +0100

    lonely

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Yello {{ name }}</h1>
+
+        <h1 v-for="item of frameworks">{{ item }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name','frameworks']
+}
+</script>
